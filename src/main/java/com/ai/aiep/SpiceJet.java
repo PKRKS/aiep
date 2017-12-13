8d9f5d7f46be09b0fa7ca3ce2f9a5115fe260d8c
Rajeshwar Kottala <rajeshwar.kottala@rackspace.com>
2017-12-12 22:36:17 -0600

    test

--- src/main/java/com/ai/aiep/SpiceJet.java
+++ src/main/java/com/ai/aiep/SpiceJet.java
@@ -14,7 +14,7 @@ public class SpiceJet {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+	//my name is rajk	
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\SUHITHREDDY\\Downloads\\chromedriver_win32//chromedriver.exe");
 		
 		WebDriver driver=new ChromeDriver();
